722d616b5dacc8c53831bcb882b9f59d46bea196
Mark Atherton <markatherton@talkholiday.com>
2021-08-23 12:37:20 +0100

    added blank use effect hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
+  const StoredLoggedInUser = localStorage.getItem("isLoggedIn");
+
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  if (StoredLoggedInUser) {
+    setIsLoggedIn(true);
+  }
+
+  useEffect(() => {}, []);
 
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
