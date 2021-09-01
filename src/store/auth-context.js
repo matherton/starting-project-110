7efda6749867332b961f59bf23f4dfb138e286b7
Mark Atherton <markatherton@talkholiday.com>
2021-09-01 11:20:20 +0100

    end of lesson 124: created auth-context to handle all state management and removed no longer required props

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,8 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const AuthContext = React.createContext({
   isLoggedIn: false,
   onLogout: () => {},
+  onLogin: (email, password) => {},
 });
+
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const StoredLoggedInUser = localStorage.getItem("isLoggedIn");
+
+    if (StoredLoggedInUser) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+
+  const loginHandler = () => {
+    localStorage.setItem("isLoggedIn", "1");
+    isLoggedIn(true);
+  };
+  return (
+    <AuthContext.Provider
+      isLoggedIn={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
